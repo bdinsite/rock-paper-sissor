92b832661d92cd0ab1c3b1e8778f5c0db32ab9a7
bdinsite <bdinsight.oy@gmail.com>
2023-09-07 10:12:48 +0300

    Remove most of old js and create new

--- cji/js/script.js
+++ cji/js/script.js
@@ -1,35 +1,159 @@
-// Get user input
-function userInput() {
-    let response;
-    while(!response) {
-        let test = prompt('Select your weapon - Rock, Paper or Sissors:').toLowerCase();
-        if(test==='rock'||test==='paper'||test==='scissors'){
-            response = test;
-        }
-    } return response;
-}
+// ADD GAME VARIABLES
+let rounds = 0;
+let playerWins = 0;
+let computerWins = 0;
+let tieRounds = 0;
 
-// Generate random RPS -value
+// GENERATE RANDOM COMPUTER WEAPON
 function getComputerChoice() {
     let randomNumber = Math.floor((Math.random()/3)*10)+1;
     let computerInput;
+    const computerImg = document.querySelector('.computer');
     if(randomNumber === 1) {
         computerInput = 'rock';
+        computerImg.src = "./cji/img/rock.png";
+        computerImg.alt = "rock";
     } else if(randomNumber === 2) {
         computerInput = 'paper';
+        computerImg.src = "./cji/img/paper.png";
+        computerImg.alt = "paper";
     } else {
-        computerInput = 'scissors'
+        computerInput = 'scissors';
+        computerImg.src = "./cji/img/scissors.png";
+        computerImg.alt = "scissors";
     };
     return computerInput;
 }
 
-// Store values and run single test round
-/* const playerSelection = userInput();
-const computerSelection = getComputerChoice();
 
-console.log(playerSelection);
-console.log(computerSelection); */
+// SELECTING WEAPONS
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const weaponSelection = document.querySelector('.weaponSelection');
 
+const scorePlayer = document.querySelector('.playerScore');
+const scoreComputer = document.querySelector('.computerScore');
+const scoreTie = document.querySelector('.tieScore');
+const fightMessage = document.querySelector('.fight');
+
+const playerImage = document.querySelector('.player');
+const computerImg = document.querySelector('.computer');
+const resetGame = document.querySelector('.reset');
+
+rock.addEventListener('click', rockAction);
+paper.addEventListener('click', paperAction);
+scissors.addEventListener('click', scissorsAction);
+resetGame.addEventListener('click', reset);
+
+function reset(){
+    rounds = 0;
+    playerWins = 0;
+    computerWins = 0;
+    tieRounds = 0;
+    playerImage.src = "";
+    computerImg.src = "";
+    computerImg.alt = "";
+    fightMessage.textContent = "";
+    scoreComputer.textContent = "Computer Score:";
+    scorePlayer.textContent = "Player Score:";
+    scoreTie.textContent = "Ties:";
+    weaponSelection.style.display = 'block';
+    rock.style.display = 'block';
+    paper.style.display = 'block';
+    scissors.style.display = 'block';
+    resetGame.style.display = 'none';
+}
+
+function endGame(){
+    resetGame.style.display = 'block';
+    weaponSelection.style.display = 'none';
+    rock.style.display = 'none';
+    paper.style.display = 'none';
+    scissors.style.display = 'none';
+    if(playerWins<computerWins){
+        fightMessage.textContent = "Computer is KING!"
+    } else if(playerWins>computerWins){
+        fightMessage.textContent = "VICTO-freakin'-RY!!!"
+    } else {
+        fightMessage.textContent = "It's a TIE?!?"
+    }
+}
+
+function rockAction(evt){
+    playerImage.src = "./cji/img/rock.png";
+    getComputerChoice();
+    if(playRound('rock', computerImg.getAttribute('alt'))=="Computer wins!"){
+        computerWins++;
+        scoreComputer.textContent = `Computer Score: ${computerWins}`;
+        fightMessage.textContent = "Computer wins!";
+        rounds++;
+
+    } else if(playRound('rock', computerImg.getAttribute('alt'))=="Player wins!"){
+        playerWins++;
+        scorePlayer.textContent = `Player Score: ${playerWins}`;
+        fightMessage.textContent = "Player wins!";
+        rounds++;
+    } else {
+        tieRounds++;
+        scoreTie.textContent = `Ties: ${tieRounds}`;
+        fightMessage.textContent = "It's a bloody tie!?!";
+        rounds++;
+    };
+    if(rounds==5){
+        endGame();
+    }
+}
+
+function paperAction(evt){
+    playerImage.src = "./cji/img/paper.png";
+    getComputerChoice();
+    if(playRound('paper', computerImg.getAttribute('alt'))=="Computer wins!"){
+        computerWins++;
+        scoreComputer.textContent = `Computer Score: ${computerWins}`;
+        fightMessage.textContent = "Computer wins!";
+        rounds++;
+    } else if(playRound('paper', computerImg.getAttribute('alt'))=="Player wins!"){
+        playerWins++;
+        scorePlayer.textContent = `Player Score: ${playerWins}`;
+        fightMessage.textContent = "Player wins!";
+        rounds++;
+    } else {
+        tieRounds++;
+        scoreTie.textContent = `Ties: ${tieRounds}`;
+        fightMessage.textContent = "It's a bloody tie!?!";
+        rounds++;
+    };
+    if(rounds==5){
+        endGame();
+    }
+}
+
+function scissorsAction(evt){
+    playerImage.src = "./cji/img/scissors.png";
+    getComputerChoice();
+    if(playRound('scissors', computerImg.getAttribute('alt'))=="Computer wins!"){
+        computerWins++;
+        scoreComputer.textContent = `Computer Score: ${computerWins}`;
+        fightMessage.textContent = "Computer wins!";
+        rounds++;
+    } else if(playRound('scissors', computerImg.getAttribute('alt'))=="Player wins!"){
+        playerWins++;
+        scorePlayer.textContent = `Player Score: ${playerWins}`;
+        fightMessage.textContent = "Player wins!";
+        rounds++;
+    } else {
+        tieRounds++;
+        scoreTie.textContent = `Ties: ${tieRounds}`;
+        fightMessage.textContent = "It's a bloody tie!?!";
+        rounds++;
+    };
+    if(rounds==5){
+        endGame();
+    };
+}
+
+// FIGHT SCORE
 function playRound(player, computer) {
     if(player===computer){
         return "It's a tie!";
@@ -45,73 +169,5 @@ function playRound(player, computer) {
         return "Player wins!";
     }  else {
         return "Player wins!";
-    }
+    };
 }
-
-// console.log(playRound(playerSelection, computerSelection));
-
-// Add game variables
-let rounds = 0;
-let battles = 0;
-let playerWins = 0;
-let computerWins = 0;
-let totalVictories = 0;
-let totalLosses = 0;
-let battleResult;
-let endResult;
-
-// The Game - rounds
-function game() {
-    rounds = playerWins = computerWins = 0;
-    battles++;
-    console.log(`Battle round: ${battles}`);
-    while(rounds < 5){
-        const player = userInput();
-        const computer = getComputerChoice();
-        //console.log(player);
-        //console.log(computer);
-        let fight = playRound(player, computer);
-        if(fight==="It's a tie!"){
-            rounds++;
-        } else if(fight==="Computer wins!"){
-            rounds++;
-            computerWins++;
-        } else {
-            rounds++;
-            playerWins++;
-        }
-    } console.log(`Rounds: ${rounds}, Player: ${playerWins}, Computer: ${computerWins} Tie: ${rounds-playerWins-computerWins}`);
-
-    if(playerWins>computerWins){
-        totalVictories++;
-        battleResult = 'Player wins this battle!';
-    } else if(playerWins<computerWins){
-        totalLosses++;
-        battleResult = 'Computer wins this battle!';
-    } else if(playerWins===computerWins && playerWins !=0){
-        battleResult = 'This battle ends in a tie!';
-    } else {
-        battleResult = 'Game on!';
-    }
-    console.log(battleResult);
-} 
-
-
-// The Game - battles
-function battle() {
-    totalVictories = totalLosses = battles = 0;
-    battleResult = endResult = "";
-    while(battles < 3){
-        game();
-    }
-    if(totalVictories>totalLosses){
-        endResult = 'Yeee-haaa!! Player defeats computer!';
-    } else if(totalVictories<totalLosses){
-        endResult = 'Oh crap!? Computer defeats player!';
-    } else {
-        endResult = "it's a bloody tie!?!";
-    }
-    console.log(endResult);
-}
-
-battle();
